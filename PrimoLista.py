74a7ef0d131967de9afc9c1380a65fe8286e7e23
LuisVegas10 <engineervegas10@gmail.com>
2022-02-01 20:50:31 -0400

    Update PrimoLista.py

--- PrimoLista.py
+++ PrimoLista.py
@@ -1,20 +1,23 @@
 '''
-Este ejercicio se ingresa un numero, y determina de 0 a dicho numero, cuanto son primos
+Este ejercicio pide un numero al usuario por consola, y determina de 2 a dicho numero, cuanto son primos
 '''
 
-numeros = int(input("Ingrese un numero entero positivos: "))
-list_primo = []
+numbers = int(input("Ingrese un numero entero positivos: "))
+listPrime = []
 
-#Ciclo que determina si cada numero es primo o no, si es primo se agrega a la lista
-for i in range(numeros + 1):
-    divisores = []
-    #si un número es primo o compuesto basta con dividirlo por los números primos menores que él hasta llegar a un cociente igual o menor que el divisor.
-    for j in range(1, i + 1):
-        if i % j == 0:
-            divisores.append(j)
-    #Si ninguna de estas divisiones es exacta, el número es primo.
-    #Si alguna de las divisiones es exacta el número es compuesto y podemos interrumpir el proceso.
-    if len(divisores) == 2:
-        list_primo.append(i)
+if numbers >= 2:
+    # Done: Ciclo que determina si cada numero es primo o no, si es primo se agrega a la lista
+    for i in range(numbers + 1):
+        dividers = []
+        # si un número es primo o compuesto basta con dividirlo por los números primos menores que él hasta llegar a un cociente igual o menor que el divisor.
+        for j in range(1, i + 1):
+            if i % j == 0:
+                dividers.append(j)
+        # Si ninguna de estas divisiones es exacta, el número es primo.
+        # Si alguna de las divisiones es exacta el número es compuesto.
+        if len(dividers) == 2:
+            listPrime.append(i)
 
-print(f"Primos: {list_primo}")
+    print(f"Numeros primos: {listPrime}")
+else:
+    print("Debe ingresar un numero positivo.")
