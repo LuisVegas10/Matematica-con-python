d4bdc49748f7588603857fa4b3cd78dee778db79
LuisVegas10 <engineervegas10@gmail.com>
2022-02-01 20:50:08 -0400

    Update Primo.py

--- Primo.py
+++ Primo.py
@@ -1,25 +1,25 @@
 '''
-Este ejercicio determina si un numero es primo
+Este ejercicio determina si un numero ingresado por consola es primo.
 '''
 
-numero = int(input("Ingrese el maximo: "))
-primo = True
+number = int(input("Ingrese el numero: "))
+isPrime = True
 
-#Si es menor o igual a 1, entonces no es primo
-if numero < 2:
-    primo = False
+# DONE: Si es menor o igual a 1, entonces no es primo
+if number < 2:
+    isPrime = False
 else:
     '''
-    si un número es primo o compuesto basta con dividirlo por los números primos menores que él hasta llegar a un cociente igual o menor que el divisor.
+    si un número es primo o compuesto basta con dividirlo por los números primos menores que él hasta llegar a un cociente igual o menor que el divisor. 
     Si ninguna de estas divisiones es exacta, el número es primo.
     Si alguna de las divisiones es exacta el número es compuesto y podemos interrumpir el proceso.
     '''
-    for i in range(2, numero):
-        if (numero % i) == 0:
-            primo = False
+    for i in range(2, number):
+        if (number % i) == 0:
+            isPrime = False
             break
 
-if primo:
-    print(f"{numero} es primo")
+if isPrime:
+    print(f"El numero {number} es primo")
 else:
-    print(f"{numero} no es primo")
+    print(f"El numero {number} no es primo")
